cbf453f0ac7c4696b4b0daa341cd45108fecdc72
Klaus Werner <werkla4@web.de>
2021-05-30 23:43:58 +0200

    slideshow to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule,
